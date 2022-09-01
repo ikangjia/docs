35bdcc9133559bd250812293b4d6417bd55ce665
kangJia <ikangjia.cn@outlook.com>
2022-09-01 19:48:37 +0800

    测试

--- docs/articles/way2god/_2022-9-1/b.md
+++ docs/articles/way2god/_2022-9-1/b.md
@@ -2,6 +2,12 @@
 title: cscasc
 outline: deep
 ---
-
+<script setup>
+import ShowTime from '/docs/components/ShowTime.vue'
+</script>
 # {{ $frontmatter.title }}
 ascascsa
+## 测试 md 文件里的 vue 代码
+{{ new Date() }}
+<ShowTime/>
+
---
+++ docs/components/ShowTime.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <h3>测试 vue 代码：vue 里的标题不会出现在页面右侧目录栏，坑啊</h3>
+  {{ new Date() }}
+  <input type="text" value="hello ">
+  <button style="color: red;border-style: dashed;" @click="alert('hello')">hello</button>
+  <a href="https://baidu.com/">aaaaaaaaaaaaaaaa</a>
+</div>
+</template>
+
+<script>
+export default {
+  name: "ShowTime"
+}
+</script>
+
+<style scoped>
+
+</style>
